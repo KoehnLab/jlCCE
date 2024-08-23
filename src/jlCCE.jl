6e5c361d24d3e7665bd4ac0923750a4b9f54eedf
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2024-08-23 16:02:14 +0200

    identify spin center and get list of spin bath nuclei

--- src/jlCCE.jl
+++ src/jlCCE.jl
@@ -5,8 +5,13 @@ export SpinSystem, cce
 using AtomsIO
 using Unitful
 
+# import the jl file to read cif files 
+#   --> export: coordinates_nuclear_spins, distance_coordinates_el_nucs
+import .readCIF
+
 # physical constants
 const hbar = 1.054571817e-34 # J s -- CODATA 2022 (rounded value)
+const hbar_cgs = 1.054571817e-27 # erg s (erg = g cm^2/s^2) 
 
 # struct to hold parameters for the run
 mutable struct SpinSystem
@@ -16,7 +21,7 @@ mutable struct SpinSystem
     spin_center::String
     # index within unit cell to select spin center, if name is not unique
     spin_center_index::Int
-    ## g factor at the center (x,y,z)
+    # g factor at the center (x,y,z)
     g_factor::Vector{Float64}
     # magnetic axes of spin center (column vectors for x, y, z direction)
     magnetic_axes::Matrix{Float64}
@@ -52,17 +57,40 @@ function cce(system::SpinSystem)
     # just some output for demo purposes (delete/modify later)
     print("Coordinates will be read from: ",system.coord_file,"\n")
     # load coordinates
-    crystal = load_system(system.coord_file)
-    lattice = ustrip.(crystal.bounding_box)
-    print("lattice vectors:\n")
-    print(lattice[1],"\n")
-    print(lattice[2],"\n")
-    print(lattice[3],"\n")
+    #crystal = load_system(system.coord_file)
+    #lattice = ustrip.(crystal.bounding_box)
+    #print("lattice vectors:\n")
+    #print(lattice[1],"\n")
+    #print(lattice[2],"\n")
+    #print(lattice[3],"\n")
 
     # identify spin center
+    if system.spin_center == "V"
+        atomic_number = 23
+    elseif system.spin_center == "Cu"
+        atomic_number = 29
+    else 
+        print("Error currently only V and Cu")
+        exit()
+    end 
+
+    if system.nuc_spin_bath == "H"
+        atomic_number_nuclei = 1
+    else 
+        print("Error only proton bath")
+        exit()
+    end
+
     # get list of spin bath nuclei
+    lattice,coord_electron_spin,coords_nuclear_spins_unit_cell = 
+        get_coordinates(system.coord_file,atomic_number,atomic_number_nuclei)
+
+    cell_list = set_supercell_list(system.r_max,lattice)
     
-    # precompute A values for electron nucleus pairs (function call)
+    coordinates_nuclear_spins,distance_coordinates_el_nucs = 
+        get_bath_list(system.r_min,system.r_max,lattice,coords_nuclear_spins_unit_cell,coord_electron_spin)
+    
+        # precompute A values for electron nucleus pairs (function call)
 
     # initialize Intensity to 1. for all times
     time = collect(range(system.t_min,system.t_max,system.n_time_step))
