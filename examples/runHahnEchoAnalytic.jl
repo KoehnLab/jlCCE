0b762255e94a68595ca0dc20e42ba9fbda2487f6
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2024-09-02 16:15:51 +0200

    changed radius (r = 0-15 A)

--- examples/runHahnEchoAnalytic.jl
+++ examples/runHahnEchoAnalytic.jl
@@ -6,17 +6,17 @@ using jlCCE
 vmnt3 = SpinSystem("vmnt3.cif","V",1)  # <--- note: put this file into the folder
 
 # modify the values (SpinSystem creates a mutable object):
-voacac2.g_factor = [1.9846,1.9846,1.9846]  # isotropic here
+vmnt3.g_factor = [1.9846,1.9846,1.9846]  # isotropic here
 # defaults, no need to define here
 #voacac2.magnetic_axes = [1.0 0.0 0.0 ; 0.0 1.0 0.0 ; 0.0 0.0 1.0]
 #voacac2.nuc_spin_bath = "H"
 #voacac2.gn_spin_bath = 5.58569468
 vmnt3.B0 = [0.,0.,1.]
 vmnt3.r_min = 0.
-vmnt3.r_max = 5. # AA
+vmnt3.r_max = 15. # AA
 vmnt3.t_min = 0.
 vmnt3.t_max = 1.5e-5 # s
-vmnt3.n_time_step = 10
+vmnt3.n_time_step = 20
 
 # run
 time, intensity = cce(vmnt3)
