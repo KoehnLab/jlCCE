6a9094c0a3708ae31e2b32e47c461be48f00efb2
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2024-09-09 15:56:38 +0200

    @time

--- examples/runHahnEchoAnalytic.jl
+++ examples/runHahnEchoAnalytic.jl
@@ -1,27 +1,35 @@
 push!(LOAD_PATH,"../src")
 
 using jlCCE
+using BenchmarkTools
 
 # set system - use the simple constructor
-vmnt3 = SpinSystem("vmnt3.cif","V",1)  # <--- note: put this file into the folder
+spinsystem = SpinSystem("vmnt3.cif","V",1)  # <--- note: put this file into the folder
 
 # modify the values (SpinSystem creates a mutable object):
-vmnt3.g_factor = [1.9846,1.9846,1.9846]  # isotropic here
+spinsystem.g_factor = [2.25,2.25,2.25]  # isotropic here
+# pddbm2: g = 2.25
+# vmnt3: 1.9846,1.9846,1.9846
+
+
+
 # defaults, no need to define here
 #voacac2.magnetic_axes = [1.0 0.0 0.0 ; 0.0 1.0 0.0 ; 0.0 0.0 1.0]
 #voacac2.nuc_spin_bath = "H"
 #voacac2.gn_spin_bath = 5.58569468
-vmnt3.B0 = [0.,0.,1.]
-vmnt3.r_min = 0.
-vmnt3.r_max = 15. # AA
-vmnt3.t_min = 0.
-vmnt3.t_max = 1.5e-5 # s
-vmnt3.n_time_step = 20
+spinsystem.B0 = [0.,0.,1.]
+spinsystem.r_min = 0.
+spinsystem.r_max = 10. # AA
+spinsystem.t_min = 0.
+spinsystem.t_max = 1.5e-5 # s
+spinsystem.n_time_step = 20
+
 
 # run
-time, intensity = cce(vmnt3)
-
+@time time_hahn_echo,intensity = cce(spinsystem)
+print("\n")
 print("Our great result:\n")
-print(intensity)
+print("time of the Hahn echo: ",time_hahn_echo,"\n")
+print("Simulated intensity: ",intensity,"\n")
 print("\n")
 
