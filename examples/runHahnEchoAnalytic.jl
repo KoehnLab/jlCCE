91e7c92601e6ec40416a19a97170221571f248a4
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2024-10-08 15:05:18 +0200

    change cif to pdb

--- examples/runHahnEchoAnalytic.jl
+++ examples/runHahnEchoAnalytic.jl
@@ -4,10 +4,10 @@ using jlCCE
 using BenchmarkTools
 
 # set system - use the simple constructor
-spinsystem = SpinSystem("vmnt3.cif","V",1)  # <--- note: put this file into the folder
+spinsystem = SpinSystem("pddbm2.pdb","Pd",1)  # <--- note: put this file into the folder
 
 # modify the values (SpinSystem creates a mutable object):
-spinsystem.g_factor = [2.25,2.25,2.25]  # isotropic here
+spinsystem.g_factor = [1.9846,1.9846,1.9846]  # isotropic here
 # pddbm2: g = 2.25
 # vmnt3: 1.9846,1.9846,1.9846
 
@@ -19,17 +19,18 @@ spinsystem.g_factor = [2.25,2.25,2.25]  # isotropic here
 #voacac2.gn_spin_bath = 5.58569468
 spinsystem.B0 = [0.,0.,1.]
 spinsystem.r_min = 0.
-spinsystem.r_max = 10. # AA
+spinsystem.r_max = 20. # AA
 spinsystem.t_min = 0.
 spinsystem.t_max = 1.5e-5 # s
 spinsystem.n_time_step = 20
 
 
 # run
-@time time_hahn_echo,intensity = cce(spinsystem)
-print("\n")
-print("Our great result:\n")
-print("time of the Hahn echo: ",time_hahn_echo,"\n")
-print("Simulated intensity: ",intensity,"\n")
-print("\n")
+time_hahn_echo,intensity = cce(spinsystem)
+
+#print("\n")
+#print("Our great result:\n")
+#print("time of the Hahn echo: ",time_hahn_echo,"\n")
+#print("Simulated intensity: ",intensity,"\n")
+#print("\n")
 
