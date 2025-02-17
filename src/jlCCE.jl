085e707eafa1eaf16938848a5f10ed710302ea00
Sarah Suchaneck <suchaneck@linux3.theochem.uni-stuttgart.de>
2025-02-17 17:38:49 +0100

    update cce:get_bath_list --> but error message: LoadError: BoundsError: attempt to access 16-element Vector{StaticArraysCore.SVector{3, Float64}} at index [17]

--- src/jlCCE.jl
+++ src/jlCCE.jl
@@ -156,9 +156,11 @@ function cce(system::SpinSystem)
 
         # call function get_bath_list: determine the distance coordinates between the electron spin center
         # and the nuclear spins and the number of considered nuclear spins in the spin bath
-        distance_coordinates_el_nucs,n_nuc,distance_coordinates_el_spin_oxygen = 
+        distance_coordinates_el_nucs,n_nuc,distance_el_spin_oxygen = 
             get_bath_list(system.r_min,system.r_max,lattice,coords_nuclear_spins_unit_cell,coord_electron_spin,coords_oxygen_unit_cell)
-    
+            #print("oxygen coords: \n")
+	    #print(distance_coordinates_el_spin_oxygen)
+	    
     else   # test
         #n_nuc = 5
         distance_coordinates_el_nucs = []
