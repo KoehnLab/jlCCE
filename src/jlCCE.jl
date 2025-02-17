ca2f85e4f376eb4faf5dfcf9ed81701aeb358c8e
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2025-02-17 16:22:24 +0100

    still debugging --> func cce: get_coordinates, det_magnetic_axes

--- src/jlCCE.jl
+++ src/jlCCE.jl
@@ -130,7 +130,7 @@ function cce(system::SpinSystem)
 
         # call function get_coordinates: determine lattice of the spin system, the coordinates of the 
             # electron spin center (x,y,z) and coordinates of the nuclear spins of the unit cell     
-        lattice,coord_electron_spin,coords_nuclear_spins_unit_cell = 
+        lattice,coord_electron_spin,coords_nuclear_spins_unit_cell,coords_oxygen_unit_cell = 
             get_coordinates(system.coord_file,atomic_number_metal,system.spin_center_index,atomic_number_nuclei,det_magnetic_axes,atomic_number_oxygen)
 
         println("\n")
