a298fe8943e8f621066d5ddd27b25ffe24880310
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2025-02-17 16:03:00 +0100

    script to determine the magnetic axes based on the geometry --> example: cudbm2, mag. axes are determined using Cu and O atoms

--- src/DetermineMagneticAxes.jl
+++ src/DetermineMagneticAxes.jl
@@ -1,46 +1,51 @@
-# determine magnetic axes from the geometry 
-# test
-# Bsp mit Cudbm2 
+# module DetermineMagneticAxes 
+
+# export MagneticAxes
 
 using LinearAlgebra
+using Printf
 
+using readCIF
+
+# example: cudbm2
+
+# coordinates of Cu and O 
 Cu = ([2.979859,    1.445000,    5.902247]);
 O1 = ([3.726962,    2.999820,    6.841884]);
 O2 = ([3.992780,    1.791800,    4.268505]);
 O3 = ([2.232756,   -0.109820,    4.962609]);
 O4 = ([1.966938,    1.098200,    7.535989]);
 
-
-# determine z direction of the magnetic axes
+# determine x,y,z contribution of the magnetic axes
+ 
 #         z
 #         |
-#    O    |   O
+#    O2   |   O1
 #        Cu ------ x 
-#    O    |   O
-#         y 
-
-# distance between Cu and O1, O4 --> spannen die Molekülebene auf
-r_cuo1 = Cu - O1
-r_cuo4 = Cu - O4 
-
-# z ist orthogonal zu den beiden Vektoren
-z =  (cross(r_cuo1,r_cuo4))/norm(cross(r_cuo1,r_cuo4))
+#    O3  /    O4
+#       / 
+#      y  
 
 # middle point between O1 & O4, O3 & O4
 M_o1o4 = (O1 + O4)/2
 M_o3o4 = (O3 + O4)/2
 
-# determnine x and y
+# determnine x and y direction of the magnetic axes
 x = (M_o1o4 - Cu)/norm(M_o1o4 - Cu)
 y = (M_o3o4 - Cu)/norm(M_o3o4 - Cu)
 
-println(x)
-println(y)
-println(z)
+# determine z direction of the magnetic axes
+z =  (cross(x,y))#/norm(cross(x,y))
+norm_z = norm(z)
+println("Norm of z: ",norm_z) 
 
-# magnetix axes matrix
+# matrix of the magnetic axes
 R_m = [x y z]
-println(R_m)
+
+println("Magnetix axes: ")
+@printf " x = [%2.4f %2.4f  %2.4f]\n" x[1] x[2] x[3]
+@printf " y = [%2.4f %2.4f  %2.4f]\n" y[1] y[2] y[3]
+@printf " z = [%2.4f %2.4f  %2.4f]\n\n" z[1] z[2] z[3]
 
 # überprüfen, ob die Achsen korrekt gesetzt wurden
 det_R_m = det(R_m)
@@ -48,3 +53,14 @@ println(det_R_m)
 
 
 
+
+
+
+
+
+alpha = deg2rad(180)
+Rot_x = [1 0 0; 0 cos(alpha) -sin(alpha); 0 sin(alpha) cos(alpha)]
+
+n = z
+a = y
+Rot_n = n.*(n.*a) + cross(cos(alpha)*(cross(n,n)),n) + sin(alpha)*cross(n,a)
