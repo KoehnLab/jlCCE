47ef9d2965def587bd617c1d8928751a44e788ea
Sarah Suchaneck <suchaneck@linuxm8.theochem.uni-stuttgart.de>
2025-03-11 18:03:29 +0100

    adapted version to the changes in DetermineMagneticAxes, readCIF, jlCCE

--- examples/dependece_B0/runHahnEchoAnalytic.jl
+++ examples/dependece_B0/runHahnEchoAnalytic.jl
@@ -9,18 +9,17 @@ using BenchmarkTools
 using Printf
 
 # set system - use the simple constructor
-system = System("../cudbm2.pdb","Pd",1)
+#system = System("../cudbm2.pdb","Pd",1)
 spinsystem = SpinSystem("../cudbm2.pdb","Pd",1)  
 
 # determine magnetic axes from geometry
-system.det_mag_axes = true
-R_m = det_mag_axes(system)
+R_m = determine_mag_axes(spinsystem)
 println("R_m: ",R_m)
 spinsystem.magnetic_axes = R_m
 
 # determine the rotated magnetic field
 B0 = [0., 0., 1.]
-theta = 0.:10:360.
+theta = 0.:10:180.
 phi = 0.:10:360.
 #rot_mat = rotate_solid(deg2rad(theta),deg2rad(phi))
 #spinsystem.B0 = R_m * (rot_mat * B0) 
@@ -37,7 +36,6 @@ spinsystem.t_min = 0.
 spinsystem.t_max = 15e-6 # s
 spinsystem.n_time_step = 50
 spinsystem.use_exp = false
-spinsystem.det_mag_axes = false
 spinsystem.simulation_type="highfield_analytic"
 
 # run
@@ -50,26 +48,26 @@ df = DataFrame(theta=Float64[], phi=Float64[], Tm=Float64[])
 for i in 1:size(theta)[1] 
     for j in 1:size(phi)[1]
         rot_mat = rotate_solid(deg2rad(theta[i]),deg2rad(phi[j]))
-	spinsystem.B0 = R_m * (rot_mat * B0)
-	push!(tab_angle_theta, theta[i])
+		spinsystem.B0 = R_m * (rot_mat * B0)
+		push!(tab_angle_theta, theta[i])
         push!(tab_angle_phi, phi[j])
 	
-	# simulate intensity
+		# simulate intensity
         #print("r_max: ",r_max,"\n")   
-	time,intensity = cce(spinsystem)
+		time,intensity = cce(spinsystem)
         
- 	println("Rotation around:")
+ 		println("Rotation around:")
         @printf "theta (Y) %10.2f ° \n" theta[i]
         @printf "phi (Z)   %12.2f ° \n" phi[j]
 
-	# determine Tm
-	Tm = 2*get_decay_time(time,intensity)*10^6
-	push!(tab_Tm, Tm)
+		# determine Tm
+		Tm = 2*get_decay_time(time,intensity)*10^6
+		push!(tab_Tm, Tm)
 
-	push!(df, (theta[i], phi[j], Tm))
+		push!(df, (theta[i], phi[j], Tm))
 
-	println("Simulated intensity: ",intensity,"\n") 
-	println("Determined coherent time: ", Tm, "\n") 
+		println("Simulated intensity: ",intensity,"\n") 
+		println("Determined coherence time: ", Tm, "\n") 
 
 	#CSV.write("Hahn_echo_intensity_diffB0.csv", Tables.table([angle_theta angle_phi times intensity]))
     end
