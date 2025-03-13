abf52de0b66581d45001e147dbd484bc3ded48a2
Sarah Suchaneck <suchaneck@linux3.theochem.uni-stuttgart.de>
2025-03-13 22:48:11 +0100

    updated version -- run simulation of cudbm2 for B0 = R_mZ (Z axis)

--- examples/runHahnEchoAnalytic.jl
+++ examples/runHahnEchoAnalytic.jl
@@ -1,31 +1,34 @@
 push!(LOAD_PATH,"../src")
 
 using jlCCE
+using jlCCEtools
+using DetermineMagneticAxes
+using RotationMatrices
 using Tables, CSV
 using BenchmarkTools
 
 # set system - use the simple constructor
-spinsystem = SpinSystem("../cudbm2.pdb","Pd",1)  # <--- note: put this file into the folder
+#spinsystem = SpinSystem("cudbm2.pdb","Pd",1)  # <--- note: put this file into the folder
 
 # determine magnetic axes from geometry
+spinsystem = SpinSystem("cudbm2.pdb","Pd",1,"O",2.0)
 R_m = determine_mag_axes(spinsystem)
-println("R_m: ",R_m)
+#println("R_m: ",R_m)
 spinsystem.magnetic_axes = R_m
 
 # define the magnetic field
 B0 = [0., 0., 1.]
 theta = 0.
 phi = 0.
-rot_mat = rotate_solid(deg2rad(theta[i]),deg2rad(phi[j]))
+rot_mat = rotate_solid(deg2rad(theta),deg2rad(phi))
 spinsystem.B0 = R_m * (rot_mat * B0)
 
-
-# modify the values (SpinSystem creates a mutable object):
+# define the spinsystem for thr run of the simulation
+spinsystem = SpinSystem("cudbm2.pdb","Pd",1)
 spinsystem.s_el = 0.5
 spinsystem.g_factor = [2.051,2.051,2.258] 
 spinsystem.r_max = 35.0					 
 spinsystem.r_min = 0.
-r_max = 35
 spinsystem.r_max_bath = 10.
 spinsystem.t_min = 0.
 spinsystem.t_max = 15e-6 # s
@@ -34,13 +37,16 @@ spinsystem.use_exp = false
 spinsystem.simulation_type="highfield_analytic"
 
 # run
-# run CCE - determine the Hahn echo intensity 
+# run CCE - determine the Hahn echo intensity
 times,intensity = cce(spinsystem)
-# determine Tm
-Tm = 2*get_decay_time(time,intensity)*10^6
-#print("\n")
-#print("Simulated intensity: ",intensity,"\n")
-#print("\n")
+print("\n")
+print("Simulated intensity: ",intensity,"\n")
+print("\n")
+#println("Simulation time: ", times)
+# determine the coherence time Tm
+Tm = 2*get_decay_time(times,intensity)*10^6
+println("Simulated coherence time: ",Tm,"\n")
+
 
 
 #CSV.write("echo.csv", Tables.table([times intensity intensityNE intensityEX iCCE1 iCCE2]))
