8204eddac73091c5e341932917e1a3520c9eade2
Sarah Suchaneck <suchaneck@theochem.uni-stuttgart.de>
2025-03-13 14:47:27 +0100

    updated version -- simulation for theta,phi = 0°, BO = R_mZ

--- examples/runHahnEchoAnalytic.jl
+++ examples/runHahnEchoAnalytic.jl
@@ -5,48 +5,44 @@ using Tables, CSV
 using BenchmarkTools
 
 # set system - use the simple constructor
-spinsystem = SpinSystem("test_rotated","Pd",1)  # <--- note: put this file into the folder
+spinsystem = SpinSystem("../cudbm2.pdb","Pd",1)  # <--- note: put this file into the folder
+
+# determine magnetic axes from geometry
+R_m = determine_mag_axes(spinsystem)
+println("R_m: ",R_m)
+spinsystem.magnetic_axes = R_m
+
+# define the magnetic field
+B0 = [0., 0., 1.]
+theta = 0.
+phi = 0.
+rot_mat = rotate_solid(deg2rad(theta[i]),deg2rad(phi[j]))
+spinsystem.B0 = R_m * (rot_mat * B0)
+
 
 # modify the values (SpinSystem creates a mutable object):
-spinsystem.g_factor = [2.051,2.051,2.258]  # isotropic here
-
-# defaults, no need to define here
-spinsystem.magnetic_axes = [1.0 0.0 0.0 ; 0.0 1.0 0.0 ; 0.0 0.0 1.0]
-#spinsystem.magnetic_axes = [-0.0930991  -0.656122   0.748421; 0.423093   -0.708907  -0.567275; 0.901291    0.258756   0.343604]
-spinsystem.B0 = [0., 0., 1.] # 10 kGaus = 1 Tesla
+spinsystem.s_el = 0.5
+spinsystem.g_factor = [2.051,2.051,2.258] 
+spinsystem.r_max = 35.0					 
 spinsystem.r_min = 0.
-spinsystem.r_max = 35.
-r_max = 10
+r_max = 35
 spinsystem.r_max_bath = 10.
 spinsystem.t_min = 0.
 spinsystem.t_max = 15e-6 # s
 spinsystem.n_time_step = 50
-
-# rotation parameters
-spinsystem.theta = 90.
-spinsystem.phi = 0. 
+spinsystem.use_exp = false
+spinsystem.simulation_type="highfield_analytic"
 
 # run
-#intensity = zeros(20,size(r_max)[1])
-#for i in 1:size(r_max)[1]
-    times,intensity = cce(spinsystem)
-    #spinsystem.use_exp = false
-    times,intensityNE = cce(spinsystem)
-    spinsystem.simulation_type="highfield_analytic"
-    #times,intensityEX,iCCE1,iCCE2 = cce(spinsystem)
-    #print("\n")
-    #print("Simulated intensity: ",intensity,"\n")
-    #print("\n")
-#end
+# run CCE - determine the Hahn echo intensity 
+times,intensity = cce(spinsystem)
+# determine Tm
+Tm = 2*get_decay_time(time,intensity)*10^6
+#print("\n")
+#print("Simulated intensity: ",intensity,"\n")
+#print("\n")
 
 
 #CSV.write("echo.csv", Tables.table([times intensity intensityNE intensityEX iCCE1 iCCE2]))
 
-
-
-#print("\n")
-#print("Our great result:\n")
-#print("time of the Hahn echo: ",time_hahn_echo,"\n")
-#print("Simulated intensity: ",intensity,"\n")
-#print("\n")
 
